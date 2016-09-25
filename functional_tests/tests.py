b5790d6c2a2a13d39e86ff93941992548b8d8910
turbopro <dantepoe@gmail.com>
2016-09-25 17:20:54 -0400

    move functional_tests.py to its own su-directory

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,7 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+#import unittest
 
 class NewVisitorTest(unittest.TestCase):
     
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a coll new online to-do app.
         # She goes to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         # She notices the page title and header mention "to-do lists"
         self.assertIn('To-Do', self.browser.title)
@@ -65,7 +66,9 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    #def test_can_start_a_list_and_retrieve_it_later(self):
+
+#if __name__ == '__main__':
+#    unittest.main(warnings='ignore')
 
 
