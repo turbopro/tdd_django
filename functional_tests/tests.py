fe91630df9171a33c975fcd95469bec985cf5fa8
turbopro <dantepoe@gmail.com>
2016-09-25 17:27:57 -0400

    make functional tests an app: use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Firefox()
